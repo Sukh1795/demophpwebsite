13e2c3647060cada4900d410a5e7ef98a21af2b3
SUKHJINDER KAUR <168607896+Sukh1795@users.noreply.github.com>
2025-03-18 19:54:44 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $studentName = "Sukhjinder Kaur";
+    $studentId = "500234581";
+    $Name = "Hairsh Sharma";
+    $Id = "500224800";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab Information</title>
+</head>
+<body>
+    <h1>Student Information</h1>
+    <p>Name: <?php echo $studentName; ?></p>
+    <p>Student ID: <?php echo $studentId; ?></p>
+
+    <p>This lab is being performed without using GitHub Actions.</p>
+</body2>
+</html>
